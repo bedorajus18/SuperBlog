6137df6c5d8b85a96cf97a958c7c5cad16a6529f
bedorajus18 <84348359+bedorajus18@users.noreply.github.com>
2021-08-20 15:37:43 +0200

    Fini
    Il reste à travailler sur la vue.

--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -1,20 +1,21 @@
 <?php
 
 namespace App\Controller;
-
+use App\Repository\BlogMessageRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomepageController extends AbstractController
 {
-    /**
-     * @Route("/homepage", name="homepage")
-     */
-    public function index(): Response
-    {
-        return $this->render('homepage/index.html.twig', [
-            'controller_name' => 'HomepageController',
-        ]);
-    }
+/**
+* @Route("/", name="homepage")
+*/
+public function index(BlogMessageRepository $blogMessageRepository): Response
+{
+return $this->render('homepage/index.html.twig', [
+'controller_name' => 'HomepageController',
+'blog_messages' => $blogMessageRepository->findAll()
+]);
+}
 }
